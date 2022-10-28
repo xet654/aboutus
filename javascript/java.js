97b2a4a5d90065bcd40c58c7c31e29b5df4d34ac
xet654 <114225380+xet654@users.noreply.github.com>
2022-10-28 15:17:55 +0200

    Add files via upload

---
+++ javascript/java.js
@@ -0,0 +1,30 @@
+gsap.fromTo(".liSocial", {opacity:0,x:-750},{opacity:1,x:0,stagger:0.25});
+
+overFunction1=()=>{
+    gsap.fromTo(".liSocial-1", {x:0},{x:100,duration:0.1});
+
+}
+outFunction1=()=>{
+    gsap.fromTo(".liSocial-1",{x:100},{x:0,duration:0.1})
+}
+overFunction2=()=>{
+    gsap.fromTo(".liSocial-2", {x:0},{x:100,duration:0.1});
+
+}
+outFunction2=()=>{
+    gsap.fromTo(".liSocial-2",{x:100},{x:0,duration:0.1})
+}
+overFunction3=()=>{
+    gsap.fromTo(".liSocial-3", {x:0},{x:100,duration:0.1});
+
+}
+outFunction3=()=>{
+    gsap.fromTo(".liSocial-3",{x:100},{x:0,duration:0.1})
+}
+overFunction4=()=>{
+    gsap.fromTo(".liSocial-4", {x:0},{x:100,duration:0.1});
+
+}
+outFunction4=()=>{
+    gsap.fromTo(".liSocial-4",{x:100},{x:0,duration:0.1})
+}
